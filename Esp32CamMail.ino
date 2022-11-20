07174e42a3558d3469711cf96563032bb673f3fc
Zalkifal Khan <88394912+Kifal15@users.noreply.github.com>
2022-11-20 23:25:02 +0500

    Initial commit

---
+++ Esp32CamMail.ino
@@ -0,0 +1,309 @@
+#include "esp_camera.h"
+#include "SPI.h"
+#include "driver/rtc_io.h"
+#include "ESP32_MailClient.h"
+#include <FS.h>
+#include <SPIFFS.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+RTC_DATA_ATTR int doorOpen = 0;
+RTC_DATA_ATTR int picturesTaken = 0;
+RTC_DATA_ATTR int bootCount = 0;
+
+
+// REPLACE WITH YOUR NETWORK CRED>ENTIALS
+const char* ssid = "enter your wifi name";
+const char* password = "enter your wifi password";
+#define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
+#define TIME_TO_SLEEP  600       /* Time ESP32 will go to sleep (in seconds) */
+
+// To send Email using Gmail use port 465 (SSL) and SMTP Server smtp.gmail.com
+// YOU MUST ENABLE less secure app option https://myaccount.google.com/lesssecureapps?pli=1
+#define emailSenderAccount    "enter the email from which you want to send the mail"
+#define emailSenderPassword   "enter your emails password"
+#define smtpServer            "smtp.gmail.com"
+#define smtpServerPort        465 // enter the server port of gmail if you aint using gmail then it may vary 
+
+#define emailRecipient        "enter the email where you want to send the image"
+
+
+// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+// Possible P
+#define CAMERA_MODEL_AI_THINKER
+
+#if defined(CAMERA_MODEL_AI_THINKER)
+  #define PWDN_GPIO_NUM     32
+  #define RESET_GPIO_NUM    -1
+  #define XCLK_GPIO_NUM      0
+  #define SIOD_GPIO_NUM     26
+  #define SIOC_GPIO_NUM     27
+  
+  #define Y9_GPIO_NUM       35
+  #define Y8_GPIO_NUM       34
+  #define Y7_GPIO_NUM       39
+  #define Y6_GPIO_NUM       36
+  #define Y5_GPIO_NUM       21
+  #define Y4_GPIO_NUM       19
+  #define Y3_GPIO_NUM       18
+  #define Y2_GPIO_NUM        5
+  #define VSYNC_GPIO_NUM    25
+  #define HREF_GPIO_NUM     23
+  #define PCLK_GPIO_NUM     22
+#else
+  #error "Camera model not selected"
+#endif
+
+// The Email Sending data object contains config and data to send
+SMTPData smtpData;
+
+// Photo File Name to save in SPIFFS
+#define FILE_PHOTO "/photo.jpg"
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+ String Kifal;
+ String Kifal1 = "Someone just entered the house"  ; 
+ String Sert = "Just a normal picture"; 
+
+ 
+void setup() {
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  
+  Serial.begin(115200);
+  Serial.println();
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  
+  if (!SPIFFS.begin(true)) {
+    Serial.println("An Error has occurred while mounting SPIFFS");
+    ESP.restart();
+  }
+  else {
+    delay(500);
+    Serial.println("SPIFFS mounted successfully");
+  }
+ 
+ 
+  // Print ESP32 Local IP Address
+  Serial.print("IP Address: http://");
+  Serial.println(WiFi.localIP());
+   timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(18000);
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // Initialize camera
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+setledon();
+  capturePhotoSaveSpiffs();
+  sendPhoto();
+    
+ 
+  esp_sleep_enable_ext0_wakeup(GPIO_NUM_13, LOW);
+  Serial.println("Going to sleep now");
+  delay(1000);
+   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
+  " Seconds");
+    esp_deep_sleep_start();
+
+
+  delay(2000);
+}
+
+void loop() {
+  
+
+ 
+}
+
+// Check if photo capture was successful
+bool checkPhoto( fs::FS &fs ) {
+  File f_pic = fs.open( FILE_PHOTO );
+  unsigned int pic_sz = f_pic.size();
+  return ( pic_sz > 100 );
+}
+
+// Capture Photo and Save it to SPIFFS
+void capturePhotoSaveSpiffs( void ) {
+  
+  camera_fb_t * fb = NULL; // pointer
+  bool ok = 0; // Boolean indicating if the picture has been taken correctly
+
+  do {
+    // Take a photo with the camera
+    Serial.println("Taking a photo...");
+
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      Serial.println("Camera capture failed");
+      return;
+    }
+
+    // Photo file name
+    Serial.printf("Picture file name: %s\n", FILE_PHOTO);
+    File file = SPIFFS.open(FILE_PHOTO, FILE_WRITE);
+
+    // Insert the data in the photo file
+    if (!file) {
+      Serial.println("Failed to open file in writing mode");
+    }
+    else {
+      file.write(fb->buf, fb->len); // payload (image), payload length
+      Serial.print("The picture has been saved in ");
+      Serial.print(FILE_PHOTO);
+      Serial.print(" - Size: ");
+      Serial.print(file.size());
+      Serial.println(" bytes");
+    }
+    // Close the file
+    file.close();
+    esp_camera_fb_return(fb);
+
+    // check if file has been correctly saved in SPIFFS
+    ok = checkPhoto(SPIFFS);
+    
+
+  } while ( !ok );
+  setledoff();
+}
+
+void sendPhoto( void ) {
+  // Preparing email
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
+  }
+  
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
+
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);
+  Serial.print("DATE: ");
+  Serial.println(dayStamp);
+  // Extract time
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.print("HOUR: ");
+  Serial.println(timeStamp);
+    esp_sleep_wakeup_cause_t wakeup_reason;
+
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+ 
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0 )
+  { bootCount++;
+    doorOpen++;
+    picturesTaken++;
+    Kifal = Kifal1 +" Date " + dayStamp + " At " + timeStamp + " Door Opened "+ doorOpen +" Times " + " Boot Count " + bootCount + " Pictures taken " +picturesTaken ;
+    Serial.println("if statement");
+  }else{
+    bootCount++;
+    picturesTaken++;
+    Kifal = Sert +" Date " + dayStamp + " At " + timeStamp + " Door Opened "+ doorOpen +" Times "+ " Boot Count " + bootCount + " Pictures taken " +picturesTaken ;
+   Serial.println("else statement");
+  } 
+  
+  delay(1000);
+  Serial.println("Sending email...");
+  // Set the SMTP Server Email host, port, account and password
+  smtpData.setLogin(smtpServer, smtpServerPort, emailSenderAccount, emailSenderPassword);
+  
+  // Set the sender name and Email
+  smtpData.setSender("ESP32-CAM", emailSenderAccount);
+  
+  // Set Email priority or importance High, Normal, Low or 1 to 5 (1 is highest)
+  smtpData.setPriority("High");
+
+  // Set the subject
+  smtpData.setSubject(Kifal);
+    
+  // Set the email message in HTML format
+  smtpData.setMessage("<h2>Kifal</h2>", true);
+  // Set the email message in text format
+  //smtpData.setMessage("Photo captured with ESP32-CAM and attached in this email.", false);
+
+  // Add recipients, can add more than one recipient
+  smtpData.addRecipient(emailRecipient);
+  //smtpData.addRecipient(emailRecipient2);
+
+  // Add attach files from SPIFFS
+  smtpData.addAttachFile(FILE_PHOTO, "image/jpg");
+  // Set the storage type to attach files in your email (SPIFFS)
+  smtpData.setFileStorageType(MailClientStorageType::SPIFFS);
+
+  smtpData.setSendCallback(sendCallback);
+  
+  // Start sending Email, can be set callback function to track the status
+  if (!MailClient.sendMail(smtpData))
+    Serial.println("Error sending Email, " + MailClient.smtpErrorReason());
+
+  // Clear all data from Email object to free memory
+  smtpData.empty();
+}
+void setledon(){
+  pinMode(4,OUTPUT);
+  digitalWrite(4, HIGH);     
+}
+void setledoff(){
+  pinMode(4,OUTPUT);
+  digitalWrite(4, LOW);
+}
+// Callback function to get the Email sending status
+void sendCallback(SendStatus msg) {
+  //Print the current status
+  Serial.println(msg.info());
+}
